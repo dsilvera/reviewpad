5da24df72c23ef197a35f84676d0d558c41e637e
David Silvera <dav.silvera@gmail.com>
2023-03-27 20:22:01 +0200

    add test

--- app/src/main/java/com/test/reviewpad/MainActivity.kt
+++ app/src/main/java/com/test/reviewpad/MainActivity.kt
@@ -2,11 +2,20 @@ package com.test.reviewpad
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ViewGroup
+import android.widget.TextView
+import fr.wemoms.R
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        (0..10).forEach {
+            findViewById<ViewGroup>(android.R.id.content).addView(TextView(this).apply {
+                text = it.toString()
+            })
+        }
     }
 }
